ca72a966abbcacfe5bdfe76be9808cb2440c2c00
surajmeruva0786 <meruva24102@iiitnr.edu.in>
2025-11-05 10:56:18 +0530

    update firebase.js

--- firebase.js
+++ firebase.js
@@ -24,13 +24,13 @@ import {
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDny41-nAzLxg6zGgHM7jc0Q0QxQCkCxO0",
-  authDomain: "agriclaim1.firebaseapp.com",
-  projectId: "agriclaim1",
-  storageBucket: "agriclaim1.firebasestorage.app",
-  messagingSenderId: "1042995668917",
-  appId: "1:1042995668917:web:eb8c8251ee9ada6244dbfc",
-  measurementId: "G-T287PS5V1T",
+  apiKey: "VITE_FIREBASE_API_KEY",
+  authDomain: "VITE_FIREBASE_AUTH_DOMAIN",
+  projectId: "VITE_FIREBASE_PROJECT_ID",
+  storageBucket: "VITE_FIREBASE_STORAGE_BUCKET",
+  messagingSenderId: "VITE_FIREBASE_MESSAGING_SENDER_ID",
+  appId: "VITE_FIREBASE_APP_ID",
+  measurementId: "VITE_FIREBASE_MEASUREMENT_ID",
 };
 
 const app = initializeApp(firebaseConfig);
